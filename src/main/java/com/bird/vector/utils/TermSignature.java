5c8eb2e78dab89697718d5eb08d8b465911f8b9c
root <lxqfindwork@163.com>
2024-10-22 15:58:36 +0800

    first commit

---
+++ src/main/java/com/bird/vector/utils/TermSignature.java
@@ -0,0 +1,27 @@
+package com.bird.vector.utils;
+
+
+import com.google.common.hash.Hashing;
+import java.nio.charset.StandardCharsets;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TermSignature {
+    private static final Logger log = LoggerFactory.getLogger(TermSignature.class);
+
+    public TermSignature() {
+    }
+
+    public static long signatureTerm(String s) {
+        long ix = 0L;
+
+        try {
+            ix = Hashing.murmur3_128().hashBytes(s.getBytes(StandardCharsets.UTF_8)).asLong();
+        } catch (Exception var4) {
+            log.error("TermSignature.signatureTerm error", var4);
+        }
+
+        return ix;
+    }
+}
+
---
+++ src/test/java/com/bird/vector/KmeansTest.java
@@ -0,0 +1,17 @@
+package com.bird.vector;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @description：
+ * @author： liuxiangqian
+ * @date： 2024/10/15
+ */
+public class KmeansTest {
+    public static void main(String[] args) {
+        String csvFile = "data/test.txt";
+        Kmeans kmeans = new Kmeans(16, 100, 1024 );
+        kmeans.train(csvFile);
+        kmeans.show();
+    }
+}
